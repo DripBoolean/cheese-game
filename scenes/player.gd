23c4995e5d55c179cc90653278744f75625165aa
Simon Rhodes <simon.m.rhodes@gmail.com>
2025-01-25 13:48:15 -0500

    added player movement

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+const SPEED = 5.0
+const SCROLL_SPEED = 1.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,4 +10,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var input_dir2 := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var input_dir3 := Vector3(input_dir2.x, 0, input_dir2.y)
+	
+	position += SPEED * input_dir3 * delta
