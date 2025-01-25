b5bf99785f100dd43f9005a5fb18a75378a0eb2c
Johnethon <98053051+Johnethon@users.noreply.github.com>
2025-01-25 01:11:34 -0500

    farmers and the market p2

--- the dairy market/the_farmers.gd
+++ the dairy market/the_farmers.gd
@@ -6,26 +6,35 @@ class_name Farmers extends Node3D
 @export var farms = 0
 @export var farm_value = 0
 @export var employees_per_farm = 0
+@export var farm_maintenance = 0
 
 @export var cows = 0
 @export var cow_purchase_value = 0
+@export var cow_maintenance = 0
 
 @export var factories = 0
 @export var factory_value = 0
 @export var exployees_per_factory = 0
+@export var factory_maintenance = 0
 
 @export var employees = 0
+@export var employee_wage = 0
+
+@export var tile_height = 1
+@export var tile_width = 1
+
+@export var grid_height = 2
+@export var grid_width = 2
 
 var Expenses = 0
 var Profit = 0
-@export var goal_profit = 0
 
 var Raw_Milk_Supply = 0
 
-"""
-
-"""
 var My_Market_Value = 0
+
+var Goal_Market_Value = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for i in range(farms):
@@ -37,28 +46,80 @@ func _ready() -> void:
 	for i in range(factories):
 		pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 func place_cow(farm_to_place_cow_in : Node3D):
 	pass
 
-func place_buidling(building_to_place : PackedScene):
+func place_building(building_to_place : PackedScene):
 	pass
 
 func decide_what_to_do():
-	pass
+	"""
+	Consider options based on the following priorities
+	
+	1. How close I am to my goal market value
+	
+		- if I am far away from my goal, be more extreme
+		
+		- if I am far above my goal, be more extreme
+	
+	2. How much money I have to attempt to make more money
+	
+		a. I don't have enough money :(
+		
+			- ask for money
+			
+				refusal = get mad
+			
+			- emergency sell extra supplies
+			
+		b. I have enough money :)
+		
+			- determine what I should purchase
+			
+				step one: hire more employees
+				
+				step two: buy more cows (if not enough space for cows)
+
+	3. What supplies I have
+	
+	4. How much money I am making
+	"""
 
 ##Either ask directly for a loan 
 func ask_for_money():
 	pass
 
+
+func go_to_market():
+	"""
+	1. update market
+	
+	2. sell as much of my stock as possible
+	"""
+
 func update_finances():
 	
+	"""
+	1. update current expenses with maintinance costs
+	"""
 	
-	pass
+	"""
+	2. take away expenses and add profits to determine current change in money
+	"""
+	
+	"""
+	3. add that change to my current money
+	"""
+	
+	"""
+	4. determine current new market value
+	"""
+	
+	"""
+	5. reset expenses or profit for next update
+	"""
 
 func _on_update_time_timeout() -> void:
+	go_to_market()
 	update_finances()
 	decide_what_to_do()
--- the dairy market/the_market.gd
+++ the dairy market/the_market.gd
@@ -3,15 +3,34 @@ class_name Market extends Node3D
 @export var Demand = 0
 
 ##per Milk
-@export var Current_Price = 1
+@export var base_price = 1
+@onready var Current_Price = 1
 
-func demand_curve(quantity):
-	pass
+@export var price_scaling = 1
 
-##out
-func supply_curve(quantity):
-	pass
+
+func demand_to_price():
+	var price = log(50) - log(2*Demand)
 	
+func determine_price_of_milk():
+	"""
+	1. Take the Demand
+	
+	2. Take the Current Supply
+	
+	3. Figure this garbage out
+	
+		lower demand than supply -> price decreases 
+		
+			effects: this lowers the price, which increases the demand
+		
+		demand roughly equals supply -> price remains the same ---- player tries to maintain this
+		
+		higher demand than supply -> price increases
+		
+			effects: this increase the price, which decreases the demand
+	"""
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
