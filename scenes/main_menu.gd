88be7adf5410b31862d14922d7297b050842c55a
breadlofe <breadlofe@gmail.com>
2025-01-26 10:14:52 -0500

    credits added

---
+++ scenes/main_menu.gd
@@ -0,0 +1,23 @@
+extends CanvasLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/world_root.tscn")
+
+
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/credits.tscn")
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
