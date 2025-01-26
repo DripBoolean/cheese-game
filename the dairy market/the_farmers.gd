69ba16e652d57f49197b5fd63fd00b9984bf5d2a
Johnethon <stickman.schneller@gmail.com>
2025-01-25 20:29:47 -0500

    farmers and market 5

--- the dairy market/the_farmers.gd
+++ the dairy market/the_farmers.gd
@@ -20,6 +20,7 @@ class_name Farmers extends Node3D
 @export var factory_value = 30
 @export var employees_per_factory = 5
 @export var factory_maintenance = 3
+@export var factory_milk_production = 10
 @export var price_to_process_each_milk = 0.05
 
 var employees = 0
@@ -76,32 +77,75 @@ func _ready() -> void:
 
 #func 
 func place_cow(farm_to_place_cow_in : Node3D):
-	pass
+	cows+=1
+
+func buy_something(i_am_panicking : bool = false):
+	if not i_am_panicking:
+		if Money <= 0:
+			return
+	
+	#if I make more raw milk than I produce, buy a factory
+	if Raw_Milk_I_Produce > Milk_I_Can_Refine:
+		#buy a new factory to compensate
+		Money -= factory_value
+		Milk_I_Can_Refine += factory_milk_production
+		
+		global.The_Market.Recent_Farm_Purchases.append(factory_value)
+		global.The_Market.Recent_Changes_in_Farmers_Market_Value.append(factory_value)
+		place_building(factory_scene)
+		
+	else:
+		#figure out how much over production I have
+		var overproduction = Milk_I_Can_Refine - Raw_Milk_I_Produce
+		;
+		if overproduction > 0:
+			#buy farms or cows to make up the difference
+			if cows < farms * cows_per_farm:
+				Raw_Milk_I_Produce += 1
+				pass
+			else:
+				
+				Money -= farm_value
+				global.The_Market.Recent_Farm_Purchases.append(farm_value)
+				global.The_Market.Recent_Changes_in_Farmers_Market_Value.append(farm_value)
+				place_building(farm_scene)
+			
+		else:
+			#if I somehow precisely have no overproduction, just buy more factories lol
+			#I'll probably need them later anyway
+			Money -= factory_value
+			Milk_I_Can_Refine += factory_milk_production
+			
+			global.The_Market.Recent_Farm_Purchases.append(factory_value)
+			global.The_Market.Recent_Changes_in_Farmers_Market_Value.append(factory_value)
+			place_building(factory_scene)
 
 func place_building(building_to_place : PackedScene):
 	if building_to_place == farm_scene:
+		farms+=1
+		
 		pass
+		#employees_per_factory
+	
 	elif building_to_place == factory_scene:
+		factories+=1
 		pass
 
-func investors_invested(number_of_shares : int):
+func investors_invested(amount : int):
 	"""
 	1. pay back the investors according to how much they sold 
 		or gain money based on how much they bought
 	
 	2. increase panic based on the amount
 	"""
-	var money_change = float(number_of_shares) * global.The_Market.Farmers_Market_Value / shares
-	Money += money_change
-	
-	if money_change < 0:
-		pass
-	else:
-		pass
+	#var money_change = float(number_of_shares) * global.The_Market.Farmers_Market_Value
+	Money += amount
+	Panic += (amount / global.The_Market.base_investment_cost) * 15
 	
 	
 ##Either ask directly for a loan 
-func ask_for_money():
+func ask_for_money(for_building : bool = false):
+	print("i asked for money")
 	pass
 
 func government_bought_milk(milk_amount : int):
@@ -119,19 +163,22 @@ func go_to_market():
 	
 	var money_change = actual_change * global.The_Market.Current_Price_of_Milk
 	Money += money_change
-	global.The_Market.change_Farm_Value(money_change)
-	global.The_Market.Recent_Changes_in_Farm_Value.append(money_change)
+	global.The_Market.Recent_Changes_in_Farmers_Market_Value.append(
+			money_change)
+	global.The_Market.Recent_Changes_in_Farm_Profit.append(money_change)
 
 func produce_milk():
 	
 	for cow in range(cows):
 		Raw_Milk_Supply += cows * 1
+		
+	Raw_Milk_I_Produce = cows
 	
 	var total_milk_made = 0
 	for factory in my_factories:
 		var new_Milk = (factory.Milk_Production *
 						factory.employees_working_here / employees_per_factory)
-		
+		Milk_I_Can_Refine = new_Milk
 		if (Raw_Milk_Supply - new_Milk) <= 0:
 			Milk_Supply += Raw_Milk_Supply
 			total_milk_made += Raw_Milk_Supply
@@ -141,17 +188,21 @@ func produce_milk():
 		total_milk_made += new_Milk
 	
 	Money -= total_milk_made * price_to_process_each_milk
-	global.The_Market.Recent_Changes_in_Farm_Value.append(
+	global.The_Market.Recent_Changes_in_Farmers_Market_Value.append(
 			total_milk_made * price_to_process_each_milk)
-
+	global.The_Market.Recent_Changes_in_Farm_Profit.append(
+			total_milk_made * price_to_process_each_milk)
 
 func take_expenses():
 	
-	Money -= (farm_maintenance * farms + 
+	var expenses = (farm_maintenance * farms + 
 				factory_maintenance * factories +
 				employees * employee_wage)
-
-
+	Money -= expenses
+	global.The_Market.Recent_Changes_in_Farmers_Market_Value.append(expenses)
+	global.The_Market.Recent_Changes_in_Farm_Profit.append(
+			expenses)
+	
 func decide_what_to_do():
 	
 	
@@ -171,6 +222,13 @@ func decide_what_to_do():
 	##Chance I have to pick a more panicked option
 	var Panic_Ratio = Panic / Max_Panic
 	
+	Panic += ((2 - ratio_of_time_left - cur_value_to_desired_value) 
+					* $decision_time.wait_time) * 1
+	
+	$decision_time.wait_time *=  $decision_time.wait_time * (1 - Panic_Ratio)
+	$decision_time.wait_time = clamp($decision_time.wait_time, 1, 3)
+	
+					
 	if cur_value_to_desired_value < 1:
 		
 		##I need to make more money
@@ -179,36 +237,44 @@ func decide_what_to_do():
 		
 		if Money > 0:
 			#yes I have money to burn
+			#buy something a random number of times
 			
-			#if I have raw milk, I should not have raw milk
-			if Raw_Milk_Supply > 0:
-				#buy a new factory to compensate
-				pass
+			for i in range(randi_range(1, 3)):
+				buy_something()
+			
+			#if milk production is less than demand, buy an extra building
+			if global.The_Market.Demand < Milk_I_Can_Refine:
+				buy_something(true)
+		else:
+			if Panic >= 80:
+				#if I am REALLY panicked for value, just buy stuff anyway and ask for money
+				buy_something()
+				ask_for_money(true)
+			elif Panic < 80 and Panic >= 40:
+				#if I am kind of panicked, ask for some money or for the government to buy cheese
+				if randf() > .5:
+					buy_something()
+				else:
+					ask_for_money(true)
+			elif Panic < 40 and Panic >= 10:
+				#if I am a little panicked, do whatever
+				if randf() > .75:
+					buy_something()
+				else:
+					ask_for_money()
 			else:
-				#figure out how much over production I have
-				var overproduction = Milk_I_Can_Refine - Raw_Milk_I_Produce
-				#buy farms to make up the difference
-				if overproduction > 0:
+				#if I am not really panicked, figure out if I am in the red
+				Expenses = (farm_maintenance * farms + 
+								factory_maintenance * factories +
+								employees * employee_wage)
+				if Expenses < 0:
+					#chill out for some more money
+					#in otherwords, do nothing
 					pass
 				else:
-					#if I somehow precisely have no overproduction, just buy more factories lol
-					#I'll probably need them later anyway
-					pass
-		else:
-			#if I am REALLY panicked for value, just buy stuff anyway lol
-			
-			#if I am kind of panicked, ask for some money or for the government to buy cheese
-			
-			#if I am not really panicked, figure out if I am in the red
-			Expenses = (farm_maintenance * farms + 
-							factory_maintenance * factories +
-							employees * employee_wage)
-			if Expenses < 0:
-				#chill out for some more money
+					ask_for_money()
 				
-				pass
-			
-			pass
+
 		"""
 		Options I have:
 			
--- the dairy market/the_market.gd
+++ the dairy market/the_market.gd
@@ -1,14 +1,16 @@
 class_name Market extends Node3D
 
 @export var Demand = 150
-@export var Max_Demand = 300
+@export var Max_Demand = 10000
 @export var Min_Demand = 10
 
-@export var Fake_Demand = 0
+@export var Fake_Demand : int = 0
 
 ##per Milk
 @export var Baseline_Price_of_Milk : float = 1
 @onready var Current_Price_of_Milk : float = Baseline_Price_of_Milk
+
+@export var base_investment_amount = 150
 
 var Farmers_Market_Value = 10
 
@@ -16,8 +18,30 @@ var Last_Investment_Amount = 0
 
 var Recent_Farm_Purchases = []
 var Recent_Changes_in_Farmers_Market_Value = []
-var Recent_Changes_in_Farm_Value = []
+var Recent_Changes_in_Farm_Profit = []
 var Recent_Changes_in_Milk_Value = []
+#--- haven't done yet
+var Recent_Changes_in_Employment = []
+
+
+####-------------------------
+##INVESTMENT GARBARGE
+
+@export var weight_of_items_purchased = 20
+@export var min_items_purchased_for_max_happy = 5
+
+@export var weight_of_money_spent = 10
+
+@export var weight_of_Farmers_Market_Value = 20
+@export var min_change_for_max_happy = 300
+
+@export var weight_of_farm_profit = 10
+
+@export var weight_of_milk_value = 5
+
+@export var weight_of_emploment_changes = 30
+
+####-------------------------
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -52,22 +76,21 @@ func modulation_to_demand_change(modulation : float) -> float:
 func investment_in_farmers():
 	var amount_i_am_going_to_invest = 0
 	
-	var chance_i_invest = 50
-	var chance_i_do_nothing = 40
-	var chance_i_sell = 10
+	var chance_i_invest :float = 50
+	var chance_i_sell : float = 50
 	
 	var total_farm_purchases = 0
-	var number_of_farm_purchaes = len(Recent_Farm_Purchases)
+	var number_of_farm_purchases = len(Recent_Farm_Purchases)
 	for value in Recent_Farm_Purchases:
 		total_farm_purchases += Recent_Farm_Purchases
 	
 	Recent_Farm_Purchases.clear()
 	
 	var total_recent_farm_profits = 0
-	for value in Recent_Changes_in_Farm_Value:
+	for value in Recent_Changes_in_Farm_Profit:
 		total_recent_farm_profits += value
 		
-	Recent_Changes_in_Farm_Value.clear()
+	Recent_Changes_in_Farm_Profit.clear()
 	
 	var total_recent_Milk_Price_changes = 0
 	for value in Recent_Changes_in_Milk_Value:
@@ -76,31 +99,69 @@ func investment_in_farmers():
 	Recent_Changes_in_Milk_Value.clear()
 	
 	var Recent_Changes_in_Farmers_Market_Value = 0
+	for value in Recent_Changes_in_Farmers_Market_Value:
+		Recent_Changes_in_Farmers_Market_Value += value
+		
+	Recent_Changes_in_Farmers_Market_Value.clear()
 	
 	##change investment chances based on recent purchases
-	for i in range(number_of_farm_purchaes):
-		pass
 	
-	##
+	var invest_change_num_purchases = clamp(number_of_farm_purchases / min_change_for_max_happy, 0.5, 2)
 	
-	##change chances based on total money spent
+	if invest_change_num_purchases < 0:
+		invest_change_num_purchases = -1.0 / invest_change_num_purchases
 	
-	##change chances based on the average value changes
+	invest_change_num_purchases *= weight_of_items_purchased
+	chance_i_invest += invest_change_num_purchases
+	chance_i_sell -= invest_change_num_purchases
 	
-	##change based on the recent average 
-	"""
-	+1. Look at how much their networth has increased
+	##change investment chances based on amount spent
 	
-	+++ 2. Look at how much they have purchased recently
+	var invest_change_money_spent = clamp(total_farm_purchases / min_change_for_max_happy, 0.5, 2)
+	if invest_change_money_spent < 0:
+		invest_change_money_spent = -1.0 / invest_change_money_spent
+		
+	invest_change_money_spent *= weight_of_money_spent
+	chance_i_invest += invest_change_money_spent
+	chance_i_sell -= invest_change_money_spent
 	
-	++3. Look at the recent trend of Milk prices
+		
+	##change chances based on the changes in Milk Price
+	
+	var previous_milk_price = Current_Price_of_Milk - total_recent_Milk_Price_changes
+	
+	var invest_change_milk = clamp(Current_Price_of_Milk / previous_milk_price, 0.5, 2)
+	
+	if invest_change_milk < 0:
+		invest_change_milk = -1.0 / invest_change_milk
+	
+	invest_change_milk *= weight_of_milk_value
+	chance_i_invest += invest_change_milk
+	chance_i_sell -= invest_change_milk
+	
+	##change chances based on the changes in Farmers Value
+	var invest_change_value = clamp(Recent_Changes_in_Farmers_Market_Value / min_change_for_max_happy, 0.5, 2)
+	
+	if invest_change_value < 0:
+		invest_change_value = -1.0 / invest_change_value
+	
+	invest_change_value *= weight_of_Farmers_Market_Value
+	chance_i_invest += invest_change_value
+	chance_i_sell -= invest_change_value
 	
 	
-	The greater these 3 factors are, the more likely the Investors invest
-		and the lower the chance is that they sell
+	##determine investment total
+	var amount = 0
+	if randf() > chance_i_invest / 100.0:
+		##they choose to invest
+		amount = base_investment_amount *  clamp(chance_i_invest / chance_i_sell, 0, 4)
+	else:
+		##they choose not to invest
+		amount = -base_investment_amount *  clamp(chance_i_sell / chance_i_invest, 0, 4)
 	
-	Sometimes they might buy or sell anyways
-	"""
+	
+	global.The_Farmers.investors_invested(amount)
+	Farmers_Market_Value += Recent_Changes_in_Farmers_Market_Value + Recent_Changes_in_Farm_Profit + Recent_Farm_Purchases
 
 func direct_demand_change(change : float):
 	pass
@@ -109,16 +170,15 @@ func direct_price_change(change : float):
 	pass
 
 
-"""
-ALTER FARMER VALUE BASED ON WHAT THEY HAVE IN INVENTORY AT THE TIME WHEN THE MILK PRICE CHANGES
-"""
 func determine_price_of_milk():
 	var demand_to_supply_ratio = 1
 	
 	if global.The_Farmers.Supply == 0:
 		demand_to_supply_ratio = 3
 	else:
-		demand_to_supply_ratio = clamp(Demand / global.The_Farmers.Supply, 0.5, 3)
+		demand_to_supply_ratio = clamp((Demand + Fake_Demand) / global.The_Farmers.Supply, 0.5, 3)
+	
+	Fake_Demand = clamp(Fake_Demand/2 - 50, 0, 100000)
 	
 	var price_modulation : float = ratio_to_price_modulation(demand_to_supply_ratio)
 	
@@ -135,8 +195,12 @@ func determine_price_of_milk():
 	Demand *= demand_change
 	Demand = clamp(Demand, Min_Demand, Max_Demand)
 	
-	#CUrrent
-
+	var current_supply_worth = global.The_Farmers.Milk_Supply * Current_Price_of_Milk
+	var previous_supply_worth = global.The_Farmers.Milk_Supply * prev_price
+	Farmers_Market_Value -= previous_supply_worth
+	Farmers_Market_Value += current_supply_worth
+	
+	Recent_Changes_in_Farmers_Market_Value.append(current_supply_worth - previous_supply_worth)
 	"""	
 	make an equation that modulates the current price by the factor generated below
 	
